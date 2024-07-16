93a96b227ffd4b008e21d572660660727d8e6553
we-r-all-earthlings <163972595+we-r-all-earthlings@users.noreply.github.com>
2024-07-16 13:45:35 +0100

    added to json file

--- src/App.jsx
+++ src/App.jsx
@@ -16,13 +16,13 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite and React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Edit the <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
