75a9e67ba40e92152af12c3b5a00f06ab4bd68f9
we-r-all-earthlings <163972595+we-r-all-earthlings@users.noreply.github.com>
2024-07-18 17:07:52 +0100

    added images

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -9,25 +7,20 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="#" target="_blank">
+          <img src="./src/images/jukebox-1.jpg" className="logo" alt="jammin logo" />
         </a>
       </div>
-      <h1>Vite and React</h1>
+      <h1>The Jammin Playlist App</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit the <code>src/App.jsx</code> and save to test HMR
+          Select your songs and create a playlist.
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>Hello World</p>
     </>
   )
 }
