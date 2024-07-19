60e001b8bc48294a74b9496842c53b6aac1956b1
we-r-all-earthlings <163972595+we-r-all-earthlings@users.noreply.github.com>
2024-07-19 19:23:04 +0100

    setting up the app areas

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,56 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const clientId = "824bc22a4f414f5ab4a7270228005dbe";
+  const redirectUri = "http://localhost:5173/jammin-app/";
+  const authEndpointUri = "https://accounts.spotify.com/authorize";
+  const responseType = "token";
+
+  const [token, setToken] = useState('initialToken');  
+
+  // const logout = () = {
+
+  // }
 
   return (
     <>
-      <div>
+      <div className="card">      
         <img src="./src/images/jukebox-1.png" className="logo" alt="jammin logo" />
+        <h1>The Jammin Playlist App</h1>
+        <p>
+          Login to Spotify.
+        </p>
+        <button href={`${authEndpointUri}?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=${responseType}`}>Spotify Login</button>
       </div>
-      <h1>The Jammin Playlist App</h1>
+      <hr></hr>
+      <div className="card">      
+        <p>
+          Search for songs and create a playlist.
+        </p>
+        <form>
+          <input label="Search the Spotify archive" ></input>
+          <button type="submit">Search</button>
+        </form>
+      </div>
+      <hr></hr>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
           Select your songs and create a playlist.
-        </p>
+        </p>        
+        <button onClick={"#"}>
+          Save to Spotify
+        </button>
       </div>
-      <p>Hello World</p>
+      <hr></hr>
+      <div className="card">
+        <p>Logout of Spotify</p>
+        <button onClick={"logout"}>
+          Logout
+        </button>
+      </div>
+
     </>
   )
 }
